ac1e22ab6311323a5940fdef720342ffa45b8c30
Gabby <loveanimalz60@gmail.com>
2026-05-26 16:56:31 -0500

    chessboard attempt

--- practice-php/chessboard.php
+++ practice-php/chessboard.php
@@ -1,2 +1,13 @@
 <?php
-    
+
+    $size = 6;
+    for($x = 0; $x <= $size; $x++) {
+        for($j = 0; $j <=$size; $j++) {
+            if ($j % 2 == 0) {
+                echo "_";
+            } else {
+                echo "=";
+            }
+        }
+        echo "<br>";
+    }
